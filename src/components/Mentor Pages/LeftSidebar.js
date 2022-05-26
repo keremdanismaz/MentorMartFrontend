190c782176580ab41a87430f28514ce3c2c0989b
KEREM DANIŞMAZ <danismazkerem@outlook.com>
2022-05-27 01:50:28 +0300

    remove icon link.

--- src/components/Mentor Pages/LeftSidebar.js
+++ src/components/Mentor Pages/LeftSidebar.js
@@ -9,7 +9,7 @@ export default class LeftSidebar extends Component {
                <div class="border-right " id="sidebar-wrapper">
 
                <div class="sidebar-heading"><i class="fas fa-city"></i>
-                   <Link to="/"> MentorMart</Link>
+                    MentorMart
                </div>
 
                <div class="list-group list-group-flush">
